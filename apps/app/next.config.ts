c9f4cf7c77c1c624c70db831914d2281322ef92b
jgillern <j.gillern@gmail.com>
2026-07-22 16:11:46 +0200

    fix: allow shareable dynamic filter URLs

--- apps/app/next.config.ts
+++ apps/app/next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   poweredByHeader: false,
   reactStrictMode: true,
-  typedRoutes: true,
   async headers() {
     return [
       {
