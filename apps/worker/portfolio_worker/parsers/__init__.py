f907496207d3e57714a4ba64a7b298c48a3b640d
jgillern <j.gillern@gmail.com>
2026-07-23 11:41:06 +0200

    Export the native George PDF parser

--- apps/worker/portfolio_worker/parsers/__init__.py
+++ apps/worker/portfolio_worker/parsers/__init__.py
@@ -1,5 +1,6 @@
+from .george_pdf import GeorgePdfParser
 from .patria_html import PatriaHtmlParser
-from .statement_text import GeorgePdfParser, XtbPdfParser
+from .statement_text import XtbPdfParser
 from .xtb_csv import XtbCsvParser
 
 __all__ = [
